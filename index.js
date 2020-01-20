1bfd692295948ce1029aeca60fc1685f40c52521
José Luis Ruiz González <58498443+JoseLuRu95@users.noreply.github.com>
2020-01-20 14:25:40 -0600

    U

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ const path = require("path");
 const morgan = require("morgan");
 
 const app = express();
-const PORT_ = 3000;
 const DB_URL = "mongodb+srv://JoseLuRu95:1234@cluster0-nwvg7.mongodb.net/test?retryWrites=true&w=majority";
 
-app.set("port", process.env.PORT || PORT_);
+app.set("port", process.env.PORT || 3000);
 
 //MIDDLEWARE
 app.use(morgan("dev"));
@@ -16,9 +15,10 @@ app.use(express.json());
 app.use(cors());
 
 //ROUTES
-app.use("/", require("./routes/contacts.route"));
+app.use("/api", require("./routes/contacts.route"));
 //STATIC FILES
-app.use(express.static(path.basename(__dirname, "public")));
+app.use(express.static(__dirname));
+
 //CONECT DATABASE
 mongoose
    .connect(DB_URL, {
